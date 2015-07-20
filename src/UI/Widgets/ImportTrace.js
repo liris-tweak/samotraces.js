6017c3169ea7384d0035a29b1c830423cecfeb46
Raphael Cazenave-Leveque <raphael.cazenave@gmail.com>
2015-07-20 12:19:04 +0200

    doc update, indentation, remove global variable

--- src/UI/Widgets/ImportTrace.js
+++ src/UI/Widgets/ImportTrace.js
@@ -11,7 +11,7 @@ var Widget = require("./Widget.js");
  * @description
  * The {@link Samotraces.UI.Widgets.Basic.ImportTrace} widget is a generic
  * Widget to import a trace from a CSV file.
- * 
+ *
  * This widget currently accept the following format:
  *
  * 1. The CSV file can use either ',' or ';' as a value separator
@@ -29,213 +29,193 @@ var Widget = require("./Widget.js");
  * 5,click,target,submit
  * </pre>
  * @todo DESCRIBE THE FORMAT OF THE CSV FILE.
- * @param {String}	html_id
- *     Id of the DIV element where the widget will be
- *     instantiated
+ * @param {object}	htmlElement
+ *     The HTML element that will be used by the widget
  * @param {Samotraces.Trace} trace
  *     Trace object in which the obsels will be imported.
  */
-var ImportTrace = function(html_id,trace) {
-	// WidgetBasicTimeForm is a Widget
-	Widget.call(this,html_id);
 
-	this.trace = trace;
-
-	this.init_DOM();
+var ImportTrace = function(htmlElement, trace) {
+  "use strict";
+  
+  // ImportTrace is a Widget
+  Widget.call(this, htmlElement);
+  this.trace = trace;
+  this.init_DOM();
 };
 
 ImportTrace.prototype = {
-	init_DOM: function() {
+  init_DOM: function() {
+    "use strict";
+    
+    var p_element = document.createElement('p');
+    var text_node = document.createTextNode('Import a trace: ');
+    
+    p_element.appendChild(text_node);
+    this.input_element = document.createElement('input');
+    this.input_element.setAttribute('type', 'file');
+    this.input_element.setAttribute('name', 'csv-file[]');
+    this.input_element.setAttribute('multiple', 'true');
+    //		this.input_element.setAttribute('size',15);
+    //		this.input_element.setAttribute('value',this.timer.time);
+    p_element.appendChild(this.input_element);
 
-		var p_element = document.createElement('p');
+    //		var submit_element = document.createElement('input');
+    //		submit_element.setAttribute('type','submit');
+    //		submit_element.setAttribute('value','Import');
+    //		p_element.appendChild(submit_element);
 
-		var text_node = document.createTextNode('Import a trace: ');
-		p_element.appendChild(text_node);
+    this.form_element = document.createElement('form');
+    this.input_element.addEventListener('change', this.on_change.bind(this));
 
-		this.input_element = document.createElement('input');
-		this.input_element.setAttribute('type','file');
-		this.input_element.setAttribute('name','csv-file[]');
-		this.input_element.setAttribute('multiple','true');
-//		this.input_element.setAttribute('size',15);
-//		this.input_element.setAttribute('value',this.timer.time);
-		p_element.appendChild(this.input_element);
+    this.form_element.appendChild(p_element);
+    this.element.appendChild(this.form_element);
 
-//		var submit_element = document.createElement('input');
-//		submit_element.setAttribute('type','submit');
-//		submit_element.setAttribute('value','Import');
-//		p_element.appendChild(submit_element);
+    var button_el = document.createElement('p');
+    var a_el = document.createElement('a');
+    a_el.href = "";
+    a_el.innerHTML = "toggle console";
+    button_el.appendChild(a_el);
+    //		button_el.innerHTML = "<a href=\"\">toggle console</a>";
+    a_el.addEventListener('click', this.on_toggle.bind(this));
+    this.element.appendChild(button_el);
 
-		this.form_element = document.createElement('form');
-		this.input_element.addEventListener('change', this.on_change.bind(this));
+    this.display_element = document.createElement("div");
+    this.display_element.style.display = "none";
+    this.element.appendChild(this.display_element);
+  },
 
-		this.form_element.appendChild(p_element);
-		this.element.appendChild(this.form_element);
+  on_change: function(e) {
+    "use strict";
 
-		var button_el = document.createElement('p');
-		var a_el = document.createElement('a');
-		a_el.href = "";
-		a_el.innerHTML = "toggle console";
-		button_el.appendChild(a_el);
-//		button_el.innerHTML = "<a href=\"\">toggle console</a>";
-		a_el.addEventListener('click',this.on_toggle.bind(this));
-		this.element.appendChild(button_el);
+    var files = e.target.files;
+    var title_el, content_el;
+    for (var i = 0, file; file = files[i]; i++) {
+      title_el = document.createElement("h2");
+      title_el.appendChild(document.createTextNode(file.name));
+      this.display_element.appendChild(title_el);
+      content_el = document.createElement("pre");
+      var reader = new FileReader();
+      reader.onload = (function(el, parser, trace) {
+        return function(e) {
+          parser(e.target.result, trace);
+          el.appendChild(document.createTextNode(e.target.result));
+        };
+      })(content_el, this.parse_csv, this.trace);
+      /*			reader.onprogress = function(e) {
+      				console.log(e);
+      			};*/
+      reader.readAsText(file);
+      this.display_element.appendChild(content_el);
+    }
+  },
 
-		this.display_element = document.createElement('div');
-		this.display_element.style.display = 'none';
-		this.element.appendChild(this.display_element);
+  on_toggle: function(e) {
+    "use strict";
+    e.preventDefault();
+    if (this.display_element.style.display === "none") {
+      this.display_element.style.display = "block";
+    } else {
+      this.display_element.style.display = "none";
+    }
+    return false;
+  },
+  parse_csv: function(text, trace) {
+    "use strict";
+    //function csvToArray() from
+    // http://stackoverflow.com/questions/1293147/javascript-code-to-parse-csv-data
 
-	},
+    // This will parse a delimited string into an array of
+    // arrays. The default delimiter is the comma, but this
+    // can be overriden in the second argument.
+    function csvToArray(strData, strDelimiter) {
+      // Check to see if the delimiter is defined. If not,
+      // then default to comma.
+      strDelimiter = (strDelimiter || ",");
 
-	on_change: function(e) {
-		files = e.target.files;
-		var title_el,content_el;
-		for( var i=0, file; file = files[i]; i++) {
-			title_el = document.createElement('h2');
-			title_el.appendChild(document.createTextNode(file.name));
-			this.display_element.appendChild(title_el);
-			content_el = document.createElement('pre');
-			var reader = new FileReader();
-			reader.onload = (function(el,parser,trace) {
-				return function(e) {
-					parser(e.target.result,trace);
-					el.appendChild(document.createTextNode(e.target.result));
-				};
-			})(content_el,this.parse_csv,this.trace);
-/*			reader.onprogress = function(e) {
-				console.log(e);
-			};*/
-			reader.readAsText(file);
-			this.display_element.appendChild(content_el);		
-		}
-	},
+      // Create a regular expression to parse the CSV values.
+      var objPattern = new RegExp(
+      (
+      // Delimiters.
+      "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
 
-	on_toggle: function(e) {
-		e.preventDefault();
-		if(this.display_element.style.display == 'none') {
-			this.display_element.style.display = 'block';
-		} else {
-			this.display_element.style.display = 'none';
-		}
-		return false;
-	},
-	parse_csv: function(text,trace) {
-		
-//function CSVToArray() from 
-// http://stackoverflow.com/questions/1293147/javascript-code-to-parse-csv-data
+      // Quoted fields.
+      "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
 
-		// This will parse a delimited string into an array of
-		// arrays. The default delimiter is the comma, but this
-		// can be overriden in the second argument.
-		function CSVToArray( strData, strDelimiter ){
-			// Check to see if the delimiter is defined. If not,
-			// then default to comma.
-			strDelimiter = (strDelimiter || ",");
+      // Standard fields.
+      "([^\"\\" + strDelimiter + "\\r\\n]*))"
+      ),
+      "gi"
+      );
 
-			// Create a regular expression to parse the CSV values.
-			var objPattern = new RegExp(
-				(
-					// Delimiters.
-					"(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+      // Create an array to hold our data. Give the array
+      // a default empty first row.
+      var arrData = [[]];
 
-					// Quoted fields.
-					"(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+      // Create an array to hold our individual pattern
+      // matching groups.
+      var arrMatches = null;
 
-					// Standard fields.
-					"([^\"\\" + strDelimiter + "\\r\\n]*))"
-				),
-				"gi"
-				);
+      // Keep looping over the regular expression matches
+      // until we can no longer find a match.
+      while (arrMatches = objPattern.exec(strData)) {
 
+        // Get the delimiter that was found.
+        var strMatchedDelimiter = arrMatches[ 1 ];
 
-			// Create an array to hold our data. Give the array
-			// a default empty first row.
-			var arrData = [[]];
+        // Check to see if the given delimiter has a length
+        // (is not the start of string) and if it matches
+        // field delimiter. If id does not, then we know
+        // that this delimiter is a row delimiter.
+        if (strMatchedDelimiter.length && (strMatchedDelimiter !== strDelimiter)) {
 
-			// Create an array to hold our individual pattern
-			// matching groups.
-			var arrMatches = null;
+          // Since we have reached a new row of data,
+          // add an empty row to our data array.
+          arrData.push([]);
+        }
 
+        // Now that we have our delimiter out of the way,
+        // let's check to see which kind of value we
+        // captured (quoted or unquoted).
+        if (arrMatches[ 2 ]) {
 
-			// Keep looping over the regular expression matches
-			// until we can no longer find a match.
-			while (arrMatches = objPattern.exec( strData )){
+          // We found a quoted value. When we capture
+          // this value, unescape any double quotes.
+          var strMatchedValue = arrMatches[ 2 ].replace(
+          new RegExp("\"\"", "g"), "\"");
+        } else {
+          // We found a non-quoted value.
+          var strMatchedValue = arrMatches[ 3 ];
+        }
 
-				// Get the delimiter that was found.
-				var strMatchedDelimiter = arrMatches[ 1 ];
+        // Now that we have our value string, let's add
+        // it to the data array.
+        arrData[ arrData.length - 1 ].push(strMatchedValue);
+      }
 
-				// Check to see if the given delimiter has a length
-				// (is not the start of string) and if it matches
-				// field delimiter. If id does not, then we know
-				// that this delimiter is a row delimiter.
-				if (
-					strMatchedDelimiter.length &&
-					(strMatchedDelimiter != strDelimiter)
-					){
+      // Return the parsed data.
+      return (arrData);
+    }
 
-					// Since we have reached a new row of data,
-					// add an empty row to our data array.
-					arrData.push( [] );
-
-				}
-
-
-				// Now that we have our delimiter out of the way,
-				// let's check to see which kind of value we
-				// captured (quoted or unquoted).
-				if (arrMatches[ 2 ]){
-
-					// We found a quoted value. When we capture
-					// this value, unescape any double quotes.
-					var strMatchedValue = arrMatches[ 2 ].replace(
-						new RegExp( "\"\"", "g" ),
-						"\""
-						);
-
-				} else {
-
-					// We found a non-quoted value.
-					var strMatchedValue = arrMatches[ 3 ];
-
-				}
-
-
-				// Now that we have our value string, let's add
-				// it to the data array.
-				arrData[ arrData.length - 1 ].push( strMatchedValue );
-			}
-
-			// Return the parsed data.
-			return( arrData );
-		}
-		
-	//	console.log('fichier chargé');
-		// guessing the separator
-		var sep = text[text.search('[,;\t]')];
-		csv = CSVToArray(text,sep);
-		csv.pop(); // remove the last line... Why?...
-	//	console.log('fichier parsé');
-		csv.map(function(line) {
-			var o_attr = {};
-			o_attr.begin = line.shift();
-			o_attr.type = line.shift();
-			o_attr.attributes = {};
-			for( var i=0; i < (line.length-1)/2 ; i++) {
-				if(line[2*i] != "") {
-					o_attr.attributes[line[2*i]] = line[2*i+1];
-				}
-			}
-		//	console.log('new obsel');
-			trace.create_obsel(o_attr);
-		});		
-/*
-		var output = "";
-		csv.forEach(function(line) {
-			output += line.join(";")+" ";
-		});
-		return output;
-*/
-	}
-
+    // console.log('fichier chargé');
+    // Guessing the separator
+    var sep = text[text.search("[,;\t]")];
+    var csv = csvToArray(text, sep);
+    csv.pop(); // remove the last line... Why?...
+    //	console.log('fichier parsé');
+    csv.map(function(line) {
+      var o_attr = {};
+      o_attr.begin = line.shift();
+      o_attr.type = line.shift();
+      o_attr.attributes = {};
+      for (var i = 0; i < (line.length - 1) / 2 ; i++) {
+        if (line[2 * i] !== "") {
+          o_attr.attributes[line[2 * i]] = line[2 * i + 1];
+        }
+      }
+      trace.create_obsel(o_attr);
+    });
+  }
 };
 
 module.exports = ImportTrace;
